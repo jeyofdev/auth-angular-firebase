fc73de05a9ad60d43a7af97017b9905b2722df9d
jeyofdev <jgregoire.pro@gmail.com>
2023-09-13 22:08:18 +0200

    feat : create shared component button-social

---
+++ src/app/shared/components/button/button-social/button-social.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-button-social',
+	templateUrl: './button-social.component.html',
+	styleUrls: ['./button-social.component.scss'],
+})
+export class ButtonSocialComponent implements OnInit {
+	@Input() icon!: string;
+	@Input() type!: 'submit' | 'reset' | 'button';
+	@Input() outline!: boolean;
+	@Input() size!: 'small' | 'medium' | 'normal' | 'large';
+
+	styleClass!: string;
+
+	ngOnInit(): void {
+		this.setStyleClass();
+	}
+
+	setStyleClass() {
+		this.styleClass = 'p-button-rounded ';
+
+		if (this.outline) {
+			this.styleClass += `p-button-outlined `;
+		}
+
+		if (this.size) {
+			this.styleClass += ` size-${this.size}`;
+		}
+	}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
 import { PrimengModule } from './primeng.module';
 import { DividerComponent } from './components/divider/divider.component';
+import { ButtonSocialComponent } from './components/button/button-social/button-social.component';
 
 @NgModule({
-	declarations: [LayoutComponent, DividerComponent],
+	declarations: [LayoutComponent, DividerComponent, ButtonSocialComponent],
 	imports: [CommonModule, PrimengModule],
 	exports: [PrimengModule, LayoutComponent],
 })
